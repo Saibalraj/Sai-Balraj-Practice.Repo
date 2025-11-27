56c7937f21e2f9ab5f0308ac051a8b344cb83036
P.Sai Balraj <psaibalraj@gmail.com>
2025-11-27 16:06:40 +0530

    Add files via upload
    A simple Java command-line program that takes two integers as arguments, validates input, and prints their sum. Designed for beginners learning Java basics, command-line arguments, and numeric parsing. Includes input checks to ensure exactly two values are provided before calculation.

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,34 @@
+//This is PRE-DEFINEND
+
+// public class SumOfTwoNumbers{
+//     public static void main(String[] args) {
+//         int num1 = 10;   
+//         int num2 = 20;   
+
+//         int sum = num1 + num2;
+
+//         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+//     }
+// }
+
+
+//This is USER-DEFIEND
+
+import java.util.Scanner;
+
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum is: " + sum);
+        sc.close();
+    }
+}
